8eaa8d01650130b41fe5cd78dfebff4d2260a62a
NguyetHMM <nguyet332747@gmail.com>
2020-04-13 06:09:01 +0700

    Week2 Example

---
+++ week2/sample1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void printff(char *str)
+{
+    printf("%s\n",str);	
+}
+int main()
+{
+   /*Day la minh hoa
+chu thich trong C
+tren nhieu dong*/
+
+   printff("Hello World!\n");
+   return 23230;
+   printf("Hello again\n");
+   return 0;
+}
---
+++ week2/sample2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main()
+{
+   /*Minh hoa dung bien
+     trong chuong trinh */ 
+   int x;
+   //x = 34 ;
+   printf("x=");
+   scanf("%d",&x);
+   int y =23;
+   
+   printf("Gia tri cua %d + %d la %d\n",x,y,x+y,x,y); 
+}
